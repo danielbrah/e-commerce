10280c1a95114b76748a120a1ecc9bf8df6c7644
danielbrah <daniel.jimenezfrancisco@gmail.com>
2023-10-09 12:23:52 -0500

    Update

---
+++ src/components/Cart.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+    <h1>wassup</h1>
+</template>
+
+<style lang="scss">
+@import '../assets/sass/layouts/cart.scss'
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+import Cart from '../components/Nav.vue';
+
+const links = ref([
+    {link: '#', display: 'Collections', id: 1},
+    {link: '#', display: 'Men', id: 2},
+    {link: '#', display: 'Women', id: 3},
+    {link: '#', display: 'About', id: 4},
+    {link: '#', display: 'Contact', id: 5}
+])
+
+
+
+</script>
+
+<template>
+    <div id="nav">
+        <div id="nav__first">
+            <a href=""><img src="../assets/images/logo.svg" alt="Logo / Home"></a>
+
+            <div id="nav__links" v-for="link in links" v-bind:link="link.link" :key="link.id">
+                <a v-bind:href="link.link">{{ link.display }}</a>
+            </div>
+        </div>
+
+        <div id="nav__second"></div>
+    </div>
+</template>
+
+<style lang="scss">
+@import '../assets/sass/layouts/nav.scss'
+</style>
