99dd6112ca902ebeb0ff17a3a19ebb0f2f9776b5
danielbrah <daniel.jimenezfrancisco@gmail.com>
2023-10-12 14:32:53 -0500

    Added hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,5 +2,21 @@
 </script>
 
 <template>
-    
+    <div id="hero">
+        <div id="hero__text">
+            <h1>Sneakers, sneakers, and <br>more sneakers!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui molestias vero amet, tenetur quisquam distinctio!</p>
+
+            <div id="hero__text--buttons">
+                <button @click="scrollTo">
+                    View items
+                </button>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style lang="scss">
+    @import '../assets/sass/layouts/hero.scss';
+    @import '../assets/sass/component/button.scss';
+</style>
