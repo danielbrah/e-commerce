725b80621e81aa4d9cc6a626e63d2c20e1774012
danielbrah <daniel.jimenezfrancisco@gmail.com>
2023-10-12 22:03:16 -0500

    Added images to hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,4 +1,18 @@
 <script setup>
+import { ref } from 'vue'
+
+const shoe = ref(null)
+
+function imageLoaded(){
+    shoe.value.style.animation = 'left-shoe 550ms ease forwards';
+}
+
+// onMounted(() => {
+//     shoe.value.children[0].addEventListener('load', () =>{
+//         shoe.value.children[0]
+//     })
+// })
+
 </script>
 
 <template>
@@ -12,6 +26,11 @@
                     View items
                 </button>
             </div>
+
+            <div ref="shoe" class="shoe__container">
+                <img @load="imageLoaded" src="../assets/images/hero-left.png" alt="Left shoe" aria-hidden="true">
+            </div>
+            <img src="../assets/images/hero-right.png" alt="Right shoe" aria-hidden="true">
         </div>
     </div>
 </template>
