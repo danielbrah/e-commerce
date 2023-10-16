593ddb42b3ef0e9681455ecbfd4ed08c61cbfdee
danielbrah <daniel.jimenezfrancisco@gmail.com>
2023-10-16 11:16:21 -0500

    Styled links, logo, and cart icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -19,7 +19,8 @@ const links = ref([
     <div id="nav-wrapper" :class="{ 'active' : !heroInViewport}">
         <div id="nav">
         <div id="nav__first">
-            <a href=""><img src="../assets/images/logo.svg" alt="Logo / Home"></a>
+            <a class="nav__dark" href=""><img src="../assets/images/logo.svg" alt="Logo / Home"></a>
+            <a class="nav__light" href=""><img src="../assets/images/logo-light.png" alt="Logo / Home"></a>
 
             <div id="nav__links">
                 <a v-for="link in links" v-bind:href="link.link" :key="link.id">{{ link.display }}</a>
@@ -27,7 +28,8 @@ const links = ref([
         </div>
 
         <div id="nav__second">
-            <img src="./icons/icon-cart.svg" alt="Cart icon / Show items in cart">
+            <img class="nav__dark" src="./icons/icon-cart.svg" alt="Cart icon / Show items in cart">
+            <img class="nav__light" src="./icons/icon-cart-light.svg" alt="Cart icon / Show items in cart">
             <img src="../assets/images/image-avatar.png" alt="Profile avatar">
         </div>
     </div>
