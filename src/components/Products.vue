9b26c8bd422f024e600e1b1ce074de1d0f6666ac
danielbrah <daniel.jimenezfrancisco@gmail.com>
2023-10-24 12:35:41 -0500

    Added mock data and styled product cards

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,17 +1,37 @@
 <script setup>
     import { ref, onMounted} from 'vue'
+
+    const productData = ref()
+
+    const getData = async function(){
+        try{
+            const res = await fetch(`https://fakestoreapi.com/products?limit=5`)
+            if(!res.ok) throw new Error('Something went wrong with getting data.')
+
+            const data = await res.json()
+            console.log(data)
+            productData.value = data
+        } catch(err)
+        {
+            console.error(`Error: ${err.message}`)
+        }
+    }
+
+onMounted(() => 
+{
+    getData()
+})
 </script>
 
 <template>
     <div id="products">
         <h2 id="products__title">Products and stuff</h2>
         <div id="products__container">
-            <div class="product" v-for="product in $products" :key="product.id">
-                <!-- <img src="" alt=""> -->
-                <h2>{{ product.name }}</h2>
-                <p>{{ product.description }}</p>
+            <div class="product" v-for="product in productData" :key="product.id">
+                <img :src="product.image" alt="image">
+                <h2>{{ product.title }}</h2>
                 <button>View</button>
-                <h3>${{ product.price }}</h3>
+                <h3>${{ (product.price).toFixed(2) }}</h3>
             </div>
         </div>
     </div>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import './assets/sass/main.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { products } from './productData'
 
 const app = createApp(App)
-app.config.globalProperties.$products = products
 app.mount('#app')
