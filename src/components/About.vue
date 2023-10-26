e76692a201c3800c978a3ceb91c247ef119c60e0
danielbrah <daniel.jimenezfrancisco@gmail.com>
2023-10-26 10:39:57 -0500

    changes

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,8 +3,13 @@
 </script>
 
 <template>
+    <div id="about">
+        <div id="about__title">
+            <h1>About Us</h1>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-
+    @import '../assets/sass/pages/about.scss'
 </style>
