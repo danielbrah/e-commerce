db9d9316a70b1ae6696421688b8a8136e97ba604
danielbrah <daniel.jimenezfrancisco@gmail.com>
2023-11-01 22:41:58 -0500

    add contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,14 +1,37 @@
 <script setup>
-import { onMounted } from 'vue' 
+import { onMounted, ref } from 'vue' 
+
+const form = ref(null)
+
+const submit = (e) =>{
+    console.log('submitted')
+}
+
 
 onMounted(() => {
     window.scroll(0, 0)
+
+
 })
 </script>
 
 <template>
     <div id="contact">
+        <div id="wrapper">
+            <h1>Any Questions? Contact us.</h1>
+            <form ref="form" method="post" data-netlify="true" @submit.prevent="submit">
+                <label for="email">Email <span class="required">*</span></label>
+                <input type="email" name="email" id="email" placeholder="yourname@example.com" size="45" autocomplete="on">
 
+                <label for="subject">Subject <span class="required">*</span></label>
+                <input type="text" name="subject" id="subject" placeholder="Subject">
+
+                <label for="message">Message <span class="required">*</span></label>
+                <textarea id="message" name="message" rows="12" cols="64" placeholder="Your message here..."></textarea>
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     </div>
 
 </template>
