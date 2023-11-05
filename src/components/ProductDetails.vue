607f9a3e9e437cc4ae0f9e698487b1ee518b68bd
danielbrah <daniel.jimenezfrancisco@gmail.com>
2023-11-04 23:57:07 -0500

    Create product-details branch

--- src/components/ProductDetails.vue
+++ src/components/ProductDetails.vue
@@ -3,5 +3,24 @@
 </script>
 
 <template>
+    <div id="product__details">
+        <div id="product__details--preview__image">
+            <img src="../assets/images/image-product-1.jpg" alt="Image">
+        </div>
 
+        <div id="product__details--info">
+            <div id="product__details--info__desc">
+                <p>Sneakers Company</p>
+                <h2>Fall Limited Edition Sneakers</h2>
+                <p>These low-profile sneakers are your perfect casual wear companion.
+                    Featuring a durable rubber outer sole, they'll withstand everything
+                    the weather can offer.
+                </p>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style lang="scss">
+    @import '../assets/sass/pages/productDetails.scss';
+</style>
