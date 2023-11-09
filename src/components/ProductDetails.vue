34d5e3a6d0ef81d42e6d6f0a0b3130a57c564639
danielbrah <daniel.jimenezfrancisco@gmail.com>
2023-11-08 19:41:35 -0600

    styled quantity section and add to cart button

--- src/components/ProductDetails.vue
+++ src/components/ProductDetails.vue
@@ -1,5 +1,7 @@
 <script setup>
     import { onMounted, ref} from 'vue'
+
+    const count = ref(0)
 </script>
 
 <template>
@@ -25,6 +27,21 @@
                     Featuring a durable rubber outer sole, they'll withstand everything
                     the weather can offer.
                 </p>
+
+                <div id="product__details--info__desc__price">
+                    <h4>$125.00</h4>
+                    <h4>50%</h4>
+                    <h4>$250.00</h4>
+                </div>
+
+                <div id="product__details--info__desc__buttons">
+                    <div>
+                        <button><img src="./icons/icon-minus.svg" alt="Subtract total quantity by one"></button>
+                        <p>{{ count }}</p>
+                        <button><img src="./icons/icon-plus.svg" alt="Add total quantity by one"></button>
+                    </div>
+                    <button>Add to cart</button>
+                </div>
             </div>
         </div>
     </div>
